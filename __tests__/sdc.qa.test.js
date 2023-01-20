a97f119fed659e199f35511f0f4645c6abfef837
Andymamooo <andyderkaema@gmail.com>
2023-01-19 20:34:44 -0800

    set up database and finish etl process

---
+++ __tests__/sdc.qa.test.js
@@ -0,0 +1,8 @@
+describe('Testing Home Route', function () {
+
+  test('1+1 should equal 2', async () => {
+    const res = 2;
+    expect(res).toBe(2);
+  });
+
+});
--- jest.config.js
+++ jest.config.js
@@ -1,54 +1,6 @@
 module.exports = {
-  // projects: [
-  //   {
-  //     collectCoverage: true,
-  //     collectCoverageFrom: ["<rootDir>/client/src/**/*.{js,jsx,ts,tsx}", "!<rootDir>/node_modules/"],
-  //     coverageDirectory: '../coverage',
-  //     coverageThreshold: {
-  //       "global": {
-  //         "lines": 80,
-  //         "statements": 80
-  //       }},
-  //     testEnvironment: 'jsdom',
-  //     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  //     moduleNameMapper: {
-  //       "^image![a-zA-Z0-9$_-]+$": "GlobalImageStub",
-  //       // '^[@./a-zA-Z0-9$_-]+\\.(png)$': 'RelativeImageStub',
-  //       '^[@./a-zA-Z0-9$_-]+\\.(png|gif)$': 'RelativeImageStub',
-  //       '^.+\\.(css|less)$': '<rootDir>/config/CSSStub.js'
-  //     },
-  //     testPathIgnorePatterns: [
-  //       "/node_modules/"
-  //     ],
-  //     verbose: true
-  //   },
-  //   {
-  //     collectCoverage: true,
-  //     collectCoverageFrom: [
-  //     "<rootDir>/server/**/*.js",
-  //     "!<rootDir>/node_modules/"],
-  //     coverageDirectory: '../coverage',
-  //     coverageThreshold: {
-  //       "global": {
-  //         "lines": 80,
-  //         "statements": 80
-  //       }},
-  //     testEnvironment: 'node',
-  //     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  //     moduleNameMapper: {
-  //       "^image![a-zA-Z0-9$_-]+$": "GlobalImageStub",
-  //       // '^[@./a-zA-Z0-9$_-]+\\.(png)$': 'RelativeImageStub',
-  //       '^[@./a-zA-Z0-9$_-]+\\.(png|gif)$': 'RelativeImageStub',
-  //       '^.+\\.(css|less)$': '<rootDir>/config/CSSStub.js'
-  //     },
-  //     testPathIgnorePatterns: [
-  //       "/node_modules/"
-  //     ],
-  //     verbose: true
-  //   }
-    // ]
     collectCoverage: true,
-    collectCoverageFrom: ["<rootDir>/client/src/**/*.{js,jsx,ts,tsx}",
+    collectCoverageFrom: [
     "<rootDir>/server/**/*.js",
     "!<rootDir>/node_modules/"],
     coverageDirectory: '../coverage',
